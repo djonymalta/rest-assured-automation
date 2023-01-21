a252508a0605d9ab240382ca000659700beb667f
kalfeltzdx <djony.malta@gmail.com>
2023-01-20 21:33:59 -0300

    Fist GET request test, ClientHttp

---
+++ src/main/java/br/automationtests/entity/ClientHttp.java
@@ -0,0 +1,11 @@
+package br.automationtests.entity;
+
+public class ClientHttp {
+    public ClientHttp(){
+
+    }
+    public String baseUrl(){
+        String baseUrl = "http://bookstore.toolsqa.com";
+        return baseUrl;
+    }
+}
